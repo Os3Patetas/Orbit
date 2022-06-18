b74bc8798840c1e7542512bdcb48a2686e145ce7
yGuddha <guilhermevrodrigues123@gmail.com>
2022-06-18 18:56:46 -0300

    Added HeartContainer file structure

--- Assets/_Projeto/UI/Score/Scripts/ScoreUIBehaviour.cs
+++ Assets/_Projeto/UI/Score/Scripts/ScoreUIBehaviour.cs
@@ -2,31 +2,26 @@ using UnityEngine;
 using TMPro;
 using com.Icypeak.Orbit.Player;
 
-namespace com.Icypeak.Orbit.UI.Score
+namespace com.Icypeak.Orbit.UI
 {
     public class ScoreUIBehaviour : MonoBehaviour
     {
         TextMeshProUGUI _scoreTextComponent;
 
-        void Awake()
-        {
+        void Awake() =>
             _scoreTextComponent = GetComponent<TextMeshProUGUI>();
-        }
 
-        void OnEnable()
-        {
+
+        void OnEnable() =>
             PlayerStats.OnScoreChange += UpdateScoreText;
-        }
 
-        void OnDisable()
-        {
+
+        void OnDisable() =>
             PlayerStats.OnScoreChange -= UpdateScoreText;
-        }
 
 
-        void UpdateScoreText(int score)
-        {
+
+        void UpdateScoreText(int score) =>
             _scoreTextComponent.text = score.ToString();
-        }
     }
 }
