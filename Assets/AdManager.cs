0d6455e5fd7171250414ec4fdf63feb4eef5134e
yGuddha <guilhermevrodrigues123@gmail.com>
2022-06-22 18:55:44 -0300

    Added Banner Add

--- Assets/AdManager.cs
+++ Assets/AdManager.cs
@@ -8,6 +8,7 @@ namespace com.Icypeak.Orbit
     {
         public static AdManager Instance;
         private RewardedInterstitialAd rewardedInterstitialAd;
+        private BannerView bannerView;
 
         public void Awake()
         {
@@ -19,6 +20,35 @@ namespace com.Icypeak.Orbit
             {
                 Instance = this;
             }
+        }
+
+        public void RequestBanner()
+        {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+
+            this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+            // Called when an ad request has successfully loaded.
+            this.bannerView.OnAdLoaded += this.HandleOnAdLoaded;
+            // Called when an ad request failed to load.
+            this.bannerView.OnAdFailedToLoad += this.HandleOnAdFailedToLoad;
+            // Called when an ad is clicked.
+            this.bannerView.OnAdOpening += this.HandleOnAdOpened;
+            // Called when the user returned from the app after an ad click.
+            this.bannerView.OnAdClosed += this.HandleOnAdClosed;
+            // Called when the ad click caused the user to leave the application.
+
+            // Create an empty ad request.
+            AdRequest request = new AdRequest.Builder().Build();
+
+            this.bannerView.LoadAd(request);
         }
 
         public void Start()
@@ -35,6 +65,8 @@ namespace com.Icypeak.Orbit
             AdRequest request = new AdRequest.Builder().Build();
             // Load the rewarded ad with the request.
             RewardedInterstitialAd.LoadAd(adUnitId, request, adLoadCallback);
+            this.RequestBanner();
+
         }
 
         private void adLoadCallback(RewardedInterstitialAd ad, AdFailedToLoadEventArgs error)
@@ -49,6 +81,33 @@ namespace com.Icypeak.Orbit
                 rewardedInterstitialAd.OnPaidEvent += HandlePaidEvent;
             }
         }
+
+        public void HandleOnAdLoaded(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdLoaded event received");
+        }
+
+        public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+        {
+            MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                                + args);
+        }
+
+        public void HandleOnAdOpened(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdOpened event received");
+        }
+
+        public void HandleOnAdClosed(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdClosed event received");
+        }
+
+        public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+        {
+            MonoBehaviour.print("HandleAdLeavingApplication event received");
+        }
+
 
         private void HandleAdFailedToPresent(object sender, AdErrorEventArgs args)
         {
